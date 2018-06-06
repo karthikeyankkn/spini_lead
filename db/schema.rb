0c337f4750fc0348dedc2fc011b408449a105fec
karthikeyankkn <connecttokkn@hotmail.com>
2018-06-06 17:35:01 +0530

    added user database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180523101821) do
+ActiveRecord::Schema.define(version: 20180606105340) do
 
   create_table "leads", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20180523101821) do
     t.string "category"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
