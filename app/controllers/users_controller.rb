91419e1e42d9e88716ef1a37968bc9148ac3562e
karthikeyankkn <connecttokkn@hotmail.com>
2018-06-11 17:04:25 +0530

    edit user added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,20 @@ class UsersController < ActionController::Base
 		end
 	end
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+  	if @user.update(user_params)
+  		flash[:success] = "Your account was successfully updated"
+	  	redirect_to leads_path
+	  else
+	  	render 'edit'	
+  	end
+	end
+
 	
 	private
 	def user_params
