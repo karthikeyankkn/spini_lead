7c61ad340577ee4a50c5e00126abd66cec750f78
karthikeyankkn <connecttokkn@hotmail.com>
2018-06-14 11:16:04 +0530

    paggination added for leads page

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,6 +1,6 @@
 class LeadsController < ActionController::Base
   def index
-  	@leads = Lead.all
+  	@leads = Lead.paginate(page: params[:page], per_page: 10)
   end
 
   def new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ActionController::Base
 	
 	def index
-		@users = User.all
+		@users = User.paginate(page: params[:page], per_page: 3)
 	end
 
 	def new
